d5d91f289a5304a38da9841279571e1c53fd3321
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-22 00:57:23 +0800

    Add main app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from 'react'
-import Welcome from './components/Welcome'
+import React, { useState, useEffect } from 'react'
+import './assets/css/App.css'
+import Header from './components/Header'
+import Control from './components/Control'
 
 export default function App() {
+	/* eslint-disable no-undef */
+	const [voices, setVoices] = useState([]);
+	const [speaking, setSpeaking] = useState(false);
+
+	const isSpeaking = async () => {
+		const chromeSpeaking = await chrome.tts.isSpeaking();
+		setSpeaking(chromeSpeaking);
+	}
+
+	const getVoices = async () => {
+		const chromeVoices = await chrome.tts.getVoices();
+		setVoices(chromeVoices);
+	}
+
+	useEffect(() => {
+		getVoices();
+	}, [])
+
+	const handleSpeak = async (text, voice, rate) => {
+		await chrome.tts.speak(text, {
+			voiceName: voice,
+			rate: parseFloat(rate),
+			onEvent: function (event) {
+				if (event.type === 'end') {
+					isSpeaking();
+				}
+			}
+		});
+		isSpeaking();
+	}
+
+	const handlePause = async () => {
+		await chrome.tts.pause();
+		isSpeaking();
+	}
+
+	const handleResume = async () => {
+		await chrome.tts.resume();
+		isSpeaking();
+	}
+
+	const handleStop = async () => {
+		await chrome.tts.stop();
+		isSpeaking();
+	}
+
 	return (
-		<Welcome />
+		<div className="App">
+			<Header />
+			<Control voices={voices} speaking={speaking} onSpeak={handleSpeak} onPause={handlePause} onResume={handleResume} onStop={handleStop} />
+		</div>
 	)
 }
